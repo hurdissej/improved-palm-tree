0742690ecedcf73cc23ff33f17919526c1d7afad
Elliot <elliot.hurdiss@finbourne.com>
2020-01-03 08:47:44 +0000

    Add middleware option

---
+++ middleware/BootstrapCacheMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cache;
+using Microsoft.AspNetCore.Http;
+
+namespace middleware
+{
+    public class BootstrapCacheMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public BootstrapCacheMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, ITestCache cache)
+        {
+            await cache.BootstrapCache();
+            await _next(httpContext);
+        }
+    }
+}
---
+++ middleware/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cache;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace postconfigureservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ITestCache _testCache;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ITestCache testCache)
+        {
+            _logger = logger;
+            _testCache = testCache;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _testCache.GetItems().ToArray();
+        }
+    }
+}
